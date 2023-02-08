a0d60639be0c4e555f9254259a35ce37da57feed
Veerash Ayyagari <veerasha@truveta.com>
2023-02-08 13:36:28 -0800

    running in k8s cluster

--- backend/src/views/view.controller.ts
+++ backend/src/views/view.controller.ts
@@ -5,14 +5,18 @@ import { Request, Response } from 'express';
 export class ViewController {
   @Get()
   root(@Req() req: Request, @Res() res: Response) {
-    this.logout(process.env.RETURN_URL, res);
+    this.logout(
+      process.env.AUTH_PROXY_ENDPOINT || process.env.RETURN_URL,
+      process.env.RETURN_URL,
+      res,
+    );
   }
 
-  logout(host: string, res: Response) {
+  logout(authProxyEndpoint: string, host: string, res: Response) {
     const encodedUrl = encodeURIComponent(
       `${process.env.OAUTH2_PROXY_OIDC_ISSUER_URL}v2/logout?returnTo=${host}&client_id=${process.env.OAUTH2_PROXY_CLIENT_ID}`,
     );
-    const urlToRedirect = `${host}/oauth2/sign_out?rd=${encodedUrl}`;
+    const urlToRedirect = `${authProxyEndpoint}/oauth2/sign_out?rd=${encodedUrl}`;
     console.log('Encoded', encodedUrl);
     console.log('Url to Redirect', urlToRedirect);
     res.redirect(urlToRedirect);
